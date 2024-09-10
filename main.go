4b730f328c68f5a947efa14975a56ed480cb02b3
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-09-10 23:04:33 +0530

    add a simple server

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	server := NewServer(":8080")
+	if err := server.Start(); err != nil {
+		log.Fatal(fmt.Sprintf("Failed to start server: %v", err))
+	}
 }
--- server.go
+++ server.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	listenAddr string
+	router     *echo.Echo
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{listenAddr: listenAddr}
+}
+
+func (server *Server) setupRouter() {
+	router := echo.New()
+
+	// Middleware
+	router.POST("/hello", sayHello)
+	server.router = router
+}
+
+func (server *Server) Start() error {
+	return server.router.Start(server.listenAddr)
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
+func sayHello(c echo.Context) error {
+	user := new(User)
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, response(err, "Invalid request payload"))
+	}
+	return c.JSON(http.StatusOK, response(nil, "Hello "+user.Name))
+}
+
+func response(err error, message string) map[string]interface{} {
+	return map[string]interface{}{
+		"error":   err.Error(),
+		"message": message,
+	}
+}
